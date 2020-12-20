c42536cde1dfca8993d803a256cf680eca4a615b
msnjkmsn <71714887+msnjkmsn@users.noreply.github.com>
2020-12-20 18:29:46 +0000

    Update Enemy.cs

--- Game/Assets/Scripts/Enemy.cs
+++ Game/Assets/Scripts/Enemy.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,21 +6,33 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
 
-   
+
     private Rigidbody enemyRb;
     private GameObject player;
-    private Player play;
+    private GameObject enemy;
     public GameManager gameManager;
-    private float xrange = 25;
-    
+    private Powerup power;
    
+
     void Start()
     {
 
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
+        
         player = GameObject.Find("Player");
-        transform.position = RandomSpawnPos();
+        enemy = GameObject.Find("Enemy");
+
+
+        if (gameManager.gameIsAvtive == true)
+        {
+            if (player.transform.position.x > RandomSpawnPos().x && player.transform.position.z > RandomSpawnPos().z)
+            {
+                transform.position = RandomSpawnPos();
+            }
+
+
+        }
+
     }
 
     // Update is called once per frame
@@ -28,30 +40,41 @@ public class Enemy : MonoBehaviour
     {
 
 
+
+
         if (gameManager.gameIsAvtive == true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.020f);
+            transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.006f);
+
+
+            if (transform.position.y < 0)
+            {
+                Destroy(gameObject);
+                gameManager.GameOver();
+            }
         }
 
 
     }
 
-    Vector3 RandomSpawnPos()
-    {
-       
 
-            return new Vector3(Random.Range(-xrange, xrange), 0, Random.Range(-35,35));
-        }
+    public Vector3 RandomSpawnPos()
+    {
+
+        return new Vector3(Random.Range(-20, 20), 0, Random.Range(-8, 20));
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.CompareTag("Enemy") && gameManager.gameIsAvtive)
+        if (collision.gameObject.CompareTag("Player") )
         {
             Destroy(gameObject);
+
+
         }
-
     }
-
 
 }
