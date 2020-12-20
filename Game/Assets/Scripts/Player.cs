c7cc118749ff8bf917e47b51cc241958efafb17f
msnjkmsn <71714887+msnjkmsn@users.noreply.github.com>
2020-12-20 19:03:19 +0000

    Update Player.cs

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed ;
+    public float turnSpeed ;
     public GameManager gameManager;
     public float horInput;
     public float verticalInput;
+    public GameObject explosion;
+    public ParticleSystem dirt;
+    public AudioClip powerUpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        playerAudio = GetComponent<AudioSource>();
+        dirt.Play();
     }
 
     // Update is called once per frame
@@ -21,36 +30,55 @@ public class Player : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         horInput = Input.GetAxis("Horizontal");
 
-        if (gameManager.gameIsAvtive == true) { 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
-        transform.Translate(Vector3.right * speed * Time.deltaTime * horInput);
-    }
+        if (gameManager.gameIsAvtive == true)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+            transform.Rotate(Vector3.up, turnSpeed * horInput * Time.deltaTime);
+        }
         if (transform.position.y < -1)
         {
             Destroy(gameObject);
             gameManager.GameOver();
         }
 
+    }
+    
+     public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Power"))
+        {
 
+            playerAudio.PlayOneShot(powerUpSound, 1.0f);
+
+        }
 
     }
+
+
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        
-            if (collision.gameObject.CompareTag("Egg") && gameManager.gameIsAvtive )
-            {
-                gameManager.UpdateScore(5);       
-                
-            }
-       if (collision.gameObject.CompareTag("Enemy"))
+
+        if (collision.gameObject.CompareTag("Egg") && gameManager.gameIsAvtive)
         {
+            gameManager.UpdateScore(5);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            GameObject particle = Instantiate(explosion, transform.position, transform.rotation);
+            particle.GetComponent<ParticleSystem>().Play();
+            dirt.Stop();
             Destroy(gameObject);
             gameManager.GameOver();
 
         }
 
     }
- 
+
+  
 
 
 }
