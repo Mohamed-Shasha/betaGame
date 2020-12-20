3a25952ce18d463a6f61fd9affa3ce66bd8684e1
msnjkmsn <71714887+msnjkmsn@users.noreply.github.com>
2020-12-20 14:45:49 +0000

    firstUpload

---
+++ Game/Assets/Scripts/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private Vector3 offset = new Vector3(0, 25,-17);
+    public GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.gameIsAvtive == true)
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Powerup.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody enemyRb;
+    private GameObject player;
+    private GameObject power;
+    private Player play;
+    public bool powerup = false;
+    private GameManager gameManager;
+    private Player playerRB;
+    public GameObject explosion;
+    private float multiplier = 2;
+    private GameObject enemy;
+    void Start()
+    {
+        
+  gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+            player = GameObject.Find("Player");
+            playerRB = GameObject.Find("Player").GetComponent<Player>();
+            power = GameObject.Find("powerUp");
+            enemy = GameObject.Find("Enemy");
+
+        if (gameManager.gameIsAvtive == true)
+        {
+           
+
+         if (player.transform.position.x > RandomSpawnPos().x && player.transform.position.z > RandomSpawnPos().z)
+            {
+                transform.position = RandomSpawnPos();
+            }
+
+
+
+        }
+    }
+       
+    
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            powerup = true;
+            StartCoroutine(picked(other));
+
+        }
+
+    }
+    IEnumerator picked(Collider player)
+    {
+       
+        GameObject particle = Instantiate(explosion, transform.position, transform.rotation);
+        particle.GetComponent<ParticleSystem>().Play();
+        player.transform.localScale *= 1.4f;
+        playerRB.speed *= multiplier;
+      
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        
+        yield return new WaitForSeconds(7);
+        playerRB.speed /= multiplier;
+        player.transform.localScale /= 1.3f;
+        Destroy(gameObject);
+
+    }
+
+   
+
+    public Vector3 RandomSpawnPos()
+    {
+
+        return new Vector3(Random.Range(-14, 15), 0.5f, Random.Range(-14, 25));
+    }
+
+
+    
+
+   
+        
+
+
+
+}
+
+
+
+
+
+
+
